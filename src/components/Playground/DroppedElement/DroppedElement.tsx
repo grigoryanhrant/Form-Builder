a203cb2a88fc766e6c38c9b5e625814fbaa83700
grigoryanhrant <hgrigoryan755@gmail.com>
2023-04-04 15:54:32 +0400

    Fix: removed the option to enable edit mode if it is already enabled

--- src/components/Playground/DroppedElement/DroppedElement.tsx
+++ src/components/Playground/DroppedElement/DroppedElement.tsx
@@ -5,80 +5,85 @@ import { editModeOn, removeField } from '@store/slices/fields/fields'
 import { ElementDefinition } from './ElementDefintion'
 import { BsArrowsMove, BsTrash, GrDocumentConfig } from '@static/icons'
 import {
-    Main,
-    Wrapper,
-    Tools,
-    ToolsIcon,
-    ToolsMoveIcon,
-    Details,
-    Description,
+  Main,
+  Wrapper,
+  Tools,
+  ToolsIcon,
+  ToolsMoveIcon,
+  Details,
+  Description,
 } from './DroppedElement.styled'
 
 interface IDroppedElement {
-    isDragging: boolean
-    DroppedRef: RefObject<HTMLDivElement>
-    handlerId: Identifier | null
-    id?: string
-    type?: string
-    name?: string
-    description?: string
-    descriptionForInput?: string
-    placeholder?: string
-    required?: boolean
-    value?: string
-    editMode?: boolean
+  isDragging: boolean
+  DroppedRef: RefObject<HTMLDivElement>
+  handlerId: Identifier | null
+  id?: string
+  type?: string
+  name?: string
+  description?: string
+  descriptionForInput?: string
+  placeholder?: string
+  required?: boolean
+  value?: string
+  editMode?: boolean
 }
 
 export const DroppedElement: FC<IDroppedElement> = ({
-    isDragging,
-    DroppedRef,
-    handlerId,
-    id,
-    type,
-    name,
-    description,
-    descriptionForInput,
-    placeholder,
-    required,
-    value,
+  isDragging,
+  DroppedRef,
+  handlerId,
+  id,
+  type,
+  name,
+  description,
+  descriptionForInput,
+  placeholder,
+  required,
+  value,
+  editMode,
 }): ReactElement => {
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch()
 
-    const cardRemoveHandler = () => dispatch(removeField(id))
+  const cardRemoveHandler = () => dispatch(removeField(id))
 
-    const setEditing = () => dispatch(editModeOn(id))
+  const setEditing = () => {
+    if (!editMode) {
+      dispatch(editModeOn(id))
+    }
+  }
 
-    return (
-        <Main onClick={setEditing}>
-            <Wrapper isDragging={isDragging} ref={DroppedRef} data-handler-id={handlerId}>
-                <Tools>
-                    <ToolsIcon onClick={cardRemoveHandler}>
-                        <BsTrash />
-                    </ToolsIcon>
+  return (
+    <Main onClick={setEditing}>
+      <Wrapper isDragging={isDragging} ref={DroppedRef} data-handler-id={handlerId}>
+        <Tools>
+          <ToolsIcon onClick={cardRemoveHandler}>
+            <BsTrash />
+          </ToolsIcon>
 
-                    <ToolsIcon>
-                        <GrDocumentConfig />
-                    </ToolsIcon>
-                </Tools>
+          <ToolsIcon>
+            <GrDocumentConfig />
+          </ToolsIcon>
+        </Tools>
 
-                <ToolsMoveIcon>
-                    <BsArrowsMove />
-                </ToolsMoveIcon>
+        <ToolsMoveIcon>
+          <BsArrowsMove />
+        </ToolsMoveIcon>
 
-                <Details>
-                    <Description>{description}</Description>
+        <Details>
+          <Description>{description}</Description>
 
-                    {ElementDefinition({
-                        id,
-                        type,
-                        name,
-                        placeholder,
-                        value,
-                        descriptionForInput,
-                        required,
-                    })}
-                </Details>
-            </Wrapper>
-        </Main>
-    )
+          {ElementDefinition({
+            id,
+            type,
+            name,
+            placeholder,
+            value,
+            descriptionForInput,
+            required,
+          })}
+        </Details>
+      </Wrapper>
+    </Main>
+  )
 }
--- src/components/Playground/DroppedElement/DroppedElementMain.tsx
+++ src/components/Playground/DroppedElement/DroppedElementMain.tsx
@@ -8,78 +8,78 @@ import { DroppedElement } from './DroppedElement'
 import { DragDropCounting } from '@helpers/dragDropCounting'
 
 interface IDroppedElementParent {
-    index: number
-    moveCard: (dragIndex: number, hoverIndex: number) => void
-    elementAddress?: string
-    id?: string
-    type?: string
-    name?: string
-    description?: string
-    descriptionForInput?: string
-    placeholder?: string
-    required?: boolean
-    value?: string
-    editMode?: boolean
+  index: number
+  moveCard: (dragIndex: number, hoverIndex: number) => void
+  elementAddress?: string
+  id?: string
+  type?: string
+  name?: string
+  description?: string
+  descriptionForInput?: string
+  placeholder?: string
+  required?: boolean
+  value?: string
+  editMode?: boolean
 }
 
 export const DroppedElementMain: FC<IDroppedElementParent> = memo(
-    ({
-        type,
-        name,
-        description,
-        descriptionForInput,
-        placeholder,
-        required,
-        value,
-        elementAddress,
-        id,
-        index,
-        moveCard,
-        editMode,
-    }): ReactElement => {
-        const DroppedRef = useRef<HTMLDivElement>(null)
+  ({
+    type,
+    name,
+    description,
+    descriptionForInput,
+    placeholder,
+    required,
+    value,
+    elementAddress,
+    id,
+    index,
+    moveCard,
+    editMode,
+  }): ReactElement => {
+    const DroppedRef = useRef<HTMLDivElement>(null)
 
-        const [{ handlerId }, drop] = useDrop<TDragObject, void, { handlerId: Identifier | null }>({
-            accept: 'dropped_element',
+    const [{ handlerId }, drop] = useDrop<TDragObject, void, { handlerId: Identifier | null }>({
+      accept: 'dropped_element',
 
-            collect(monitor) {
-                return {
-                    handlerId: monitor.getHandlerId(),
-                }
-            },
+      collect(monitor) {
+        return {
+          handlerId: monitor.getHandlerId(),
+        }
+      },
 
-            hover(item: TDragObject, monitor: DropTargetMonitor<TDragObject, void>) {
-                DragDropCounting(item, monitor, DroppedRef, index, moveCard)
-            },
-        })
+      hover(item: TDragObject, monitor: DropTargetMonitor<TDragObject, void>) {
+        DragDropCounting(item, monitor, DroppedRef, index, moveCard)
+      },
+    })
 
-        const [{ isDragging }, drag] = useDrag({
-            type: 'dropped_element',
-            item: { id, index, elementAddress },
-            collect: (monitor: DragSourceMonitor<TDragObject, TDragObject>) => ({
-                isDragging: monitor.isDragging(),
-            }),
-        })
+    const [{ isDragging }, drag] = useDrag({
+      type: 'dropped_element',
+      item: { id, index, elementAddress },
+      collect: (monitor: DragSourceMonitor<TDragObject, TDragObject>) => ({
+        isDragging: monitor.isDragging(),
+      }),
+    })
 
-        drag(drop(DroppedRef))
+    drag(drop(DroppedRef))
 
-        return (
-            <DroppedElement
-                isDragging={isDragging}
-                DroppedRef={DroppedRef}
-                handlerId={handlerId}
-                id={id}
-                type={type}
-                name={name}
-                description={description}
-                descriptionForInput={descriptionForInput}
-                placeholder={placeholder}
-                required={required}
-                value={value}
-                editMode={editMode}
-            />
-        )
-    },
+    return (
+      <DroppedElement
+        isDragging={isDragging}
+        DroppedRef={DroppedRef}
+        handlerId={handlerId}
+        id={id}
+        type={type}
+        name={name}
+        description={description}
+        descriptionForInput={descriptionForInput}
+        placeholder={placeholder}
+        required={required}
+        value={value}
+        editMode={editMode}
+      />
+    )
+  },
 )
 
 DroppedElementMain.displayName = 'DroppedElementMain'
